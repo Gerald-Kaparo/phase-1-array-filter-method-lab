a96c8930e5c9ed2f78ccd86813aef2a4fa4d363d
Gerald <kapgerado05@gmail.com>
2022-10-05 15:26:53 +0300

    Done with JS array filter method-lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+const drivers = [
+//These are the objects matching names and hometown
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+//My arrow functions reffering to the above array
+
+const findMatching = (drivers, property) =>  {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === property.toLowerCase() })
+  }
+  
+  const fuzzyMatch = (drivers, property) => {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, property.length) === property.toLowerCase() })
+  }
+  
+  const matchName = (drivers, myMatchName) => {
+    return drivers.filter(function (driver) { return driver.name === myMatchName })
+  }
